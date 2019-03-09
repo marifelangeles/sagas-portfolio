f9c973ed3dd304714a409bf24b3a2a1a8c803578
Marifel Angeles <mmarifel@gmail.com>
2019-03-09 10:17:03 -0600

    add server side route to post new project

--- server/routes/project.router.js
+++ server/routes/project.router.js
@@ -3,7 +3,6 @@ const pool = require('../modules/pool');
 const router = express.Router();
 // const bodyParser = require('body-parser');
 
-// const project = ['project1,', 'project2'];
 
 router.get('/', (req, res) => {
     const queryText = `SELECT * FROM "projects";`;
@@ -13,6 +12,28 @@ router.get('/', (req, res) => {
             res.send(result.rows);
         }).catch(error => {
             console.log('error with GET /project', error);
+            res.sendStatus(500);
+        });
+});
+
+router.post('/', (req, res) => {
+    const queryText = `INSERT INTO "projects" ("name", "description",           "thumbnail", "website", "github", "date_completed", "tag_id") 
+    VALUES ($1, $2, $3, $4, $5, $6, $7);`;
+    const newProject = req.body;
+    const queryValues = [
+        newProject.name,
+        newProject.description,
+        newProject.thumbnail,
+        newProject.website,
+        newProject.github,
+        newProject.date_completed,
+        newProject.tag_id
+    ]
+    pool.query(queryText, queryValues)
+        .then(() => {
+            res.sendStatus(200);
+        }).catch(error => {
+            console.log('error with POST /project', error);
             res.sendStatus(500);
         });
 });
